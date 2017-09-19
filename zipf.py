9f75f54ec0802ddbb52275174d90db2018a9456b
mcmaceac <mcm.maceachern@gmail.com>
2017-09-19 13:05:32 -0400

    Now displaying top 10 words (descending)

--- zipf.py
+++ zipf.py
@@ -12,13 +12,24 @@ with open('stopwords-MySQL.txt', 'r') as f:		#open the file and read the content
 
 for line in sys.stdin:
 	line = line.strip()
-	words = filter(None, re.split('\W+', line))
+	words = filter(None, re.split('[^a-zA-Z0-9\']', line))		#separating on white space(had to include ' becuase it was filtering on contractions)
+	words = map(lambda x:x.lower(), words)			#convering words to lowercase
+	
 	for word in words:
-
 		if word not in stopWords:		#checking to see if the word is a stop word
+			if word[-2:] == '\'s':		#required in order to filter multiples of words
+				word = word[:-2]
 			if word not in occurences:	#the word is not already in the dictionary
 				occurences[word] = 1
 			else:
 				occurences[word] += 1	#the word is in the dictionary, increment count by 1
 
-print('%s' % str(occurences))
+#sorting by value
+
+
+topTen = sorted(occurences, key=occurences.get, reverse=True)
+topTen = topTen[:10]							
+for val in topTen:
+	print('%s\t%s' % (val, occurences[val]))
+
+#print('%s' % str(occurences))
