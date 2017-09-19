ba5de9ed320300cd3b039727a580995c4edf46e4
Matthew MacEachern <mcm.maceachern@gmail.com>
2017-09-18 21:38:35 -0400

    Implemented stop word filtering

--- zipf.py
+++ zipf.py
@@ -4,15 +4,21 @@ import sys
 import re
 
 occurences = {}
+stopWords = []
+
+with open('stopwords-MySQL.txt', 'r') as f:
+	stopWords = [line.strip() for line in f]
+
 
 for line in sys.stdin:
 	line = line.strip()
 	words = filter(None, re.split('\W+', line))
 	for word in words:
 
-		if word not in occurences:
-			occurences[word] = 1
-		else:
-			occurences[word] += 1
+		if word not in stopWords:
+			if word not in occurences:
+				occurences[word] = 1
+			else:
+				occurences[word] += 1
 
 print('%s' % str(occurences))
