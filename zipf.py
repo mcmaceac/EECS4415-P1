5acf526cae6d204c46a228dc0dfb8d2b2d484dbf
Matthew MacEachern <mcm.maceachern@gmail.com>
2017-09-18 21:40:49 -0400

    Added documentation

--- zipf.py
+++ zipf.py
@@ -6,8 +6,8 @@ import re
 occurences = {}
 stopWords = []
 
-with open('stopwords-MySQL.txt', 'r') as f:
-	stopWords = [line.strip() for line in f]
+with open('stopwords-MySQL.txt', 'r') as f:		#open the file and read the contents line
+	stopWords = [line.strip() for line in f]	#by line, adding each stripped stop word to the list
 
 
 for line in sys.stdin:
@@ -15,10 +15,10 @@ for line in sys.stdin:
 	words = filter(None, re.split('\W+', line))
 	for word in words:
 
-		if word not in stopWords:
-			if word not in occurences:
+		if word not in stopWords:		#checking to see if the word is a stop word
+			if word not in occurences:	#the word is not already in the dictionary
 				occurences[word] = 1
 			else:
-				occurences[word] += 1
+				occurences[word] += 1	#the word is in the dictionary, increment count by 1
 
 print('%s' % str(occurences))
